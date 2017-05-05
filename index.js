f03c1744ce5a5653171bc5a84787837d24050f9d
fujisawa-shin <fujisawa-shin@kayac.com>
2017-05-06 00:23:44 +0900

    とりあえずHTMLが出せた

--- index.js
+++ index.js
@@ -1,23 +1,31 @@
 const EventEmitter = require('events');
+const path = require('path');
+
 const fsp = require('fs-promise');
 const markdown = require('markdown').markdown;
+const pug = require('pug');
+
+const TEMPLATE_PUG = `${__dirname}/src/pug/index.pug`;
 
 class SectorSlide extends EventEmitter {
     constructor (opts = {}) {
         super();
-        this.markdown = opts.markdown;
+        this.documentMarkdown = opts.documentMarkdown;
         this.src = opts.src;
+        this.docroot = opts.docroot;
     }
 
     start () {
         return Promise.resolve()
-            .then(() => { return this.loadMarkdown(); })
-            .then(() => { return this.renderHTML(); });
+            .then(() => { return this.loadDocumentMarkdown(); })
+            .then(() => { return this.renderDocumentHTML(); })
+            .then(() => { return this.renderPug(); })
+            .then(() => { return this.exportWeb(); });
     }
 
-    loadMarkdown () {
+    loadDocumentMarkdown () {
         return new Promise((resolve, reject) => {
-            if (this.markdown) {
+            if (this.documentMarkdown) {
                 resolve();
                 return;
             }
@@ -28,7 +36,7 @@ class SectorSlide extends EventEmitter {
 
             fsp.readFile(this.src, { encoding: 'utf8' }).then(
                 (body) => {
-                    this.markdown = body;
+                    this.documentMarkdown = body;
                     resolve(body);
                 },
                 (err) => {
@@ -41,11 +49,51 @@ class SectorSlide extends EventEmitter {
         });
     }
     
-    renderHTML () {
+    renderDocumentHTML () {
         return new Promise((resolve, reject) => {
-            const html = markdown.toHTML(this.markdown);
-            this.html = html;
-            resolve(html);
+            this.documentHTML = markdown
+                      .toHTML(this.documentMarkdown)
+                      .replace(/\n */g, '');
+            resolve(this.documentHTML);
+        });
+    }
+
+    loadPugTemplate () {
+        return new Promise((resolve, reject) => {
+            fsp.readFile(TEMPLATE_PUG, { encoding: 'utf8' }).then(
+                resolve,
+                (err) => {
+                    reject(new Error([
+                        'fail to load pug template',
+                        err.toString()
+                    ].join('\n')));
+                }
+            );
+        });
+    }
+
+    renderPug () {
+        return new Promise((resolve, reject) => {
+            this.loadPugTemplate().then((pugTemplate) => {
+                const fn = pug.compile(pugTemplate, {
+                    pretty: false
+                });
+                this.indexHTML = fn({
+                    content: this.documentHTML
+                });
+                resolve(this.indexHTML);
+            });
+        });
+    }
+
+    exportWeb () {
+        return Promise.resolve().then(() => {
+            return fsp.mkdirp(this.docroot);
+        }).then(() => {
+            return fsp.writeFile(
+                path.resolve(this.docroot, 'index.html'),
+                this.indexHTML
+            );
         });
     }
 };
